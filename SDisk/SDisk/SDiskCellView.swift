48ad9d772b266e723155df00bd3180e4940997e8
Mayank Kumar <mayankk2308@gmail.com>
2019-01-21 17:45:32 -0500

    Prepare basic user interface

---
+++ SDisk/SDisk/SDiskCellView.swift
@@ -0,0 +1,13 @@
+//
+//  SDiskCellView.swift
+//  SDisk
+//
+//  Created by Mayank Kumar on 1/21/19.
+//  Copyright © 2019 Mayank Kumar. All rights reserved.
+//
+
+import Cocoa
+
+class SDiskCellView: NSTableCellView {
+
+}
